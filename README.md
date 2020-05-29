a614a5fb82d8f93dd82e448c186d727043955da5
Kimberly <kimberly.leonard@mjdinteractive.com>
2020-05-28 17:45:06 -0700

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Portfolio Site using React.js and CSS
-
-## Preview
-![Preview](https://user-images.githubusercontent.com/57583457/73320486-514c4500-41f4-11ea-883d-b172fb82c99b.png)
-
-## Tech
-Uses React.js; customized the original template to create my portfolio 
-
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.add738c2993383f7030a64924b8ca9c6.js"
+  "/portfolio/precache-manifest.813be2b32f4ba4bf39878bd5b99c9a52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
